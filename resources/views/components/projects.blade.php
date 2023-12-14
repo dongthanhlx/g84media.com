45642b5c2d1e2a35716e78b8cea4eb49c2077d0b
dongthanhlx <dongthanhlx@gmail.com>
2023-12-14 15:07:08 +0700

    thêm các dịch vụ

--- resources/views/components/projects.blade.php
+++ resources/views/components/projects.blade.php
@@ -5,41 +5,73 @@
         <p class="text-gray-600 px-5">Tầm vóc và tính cách của doanh nghiệp được thể hiện một cách sống động qua từng thước phim</p>
     </div>
 
-    <div class="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-10 px-10">
-        <div class="hover:shadow-xl hover:cursor-pointer">
-            <a href="https://drive.google.com/file/d/1jug_MwnbOQYaeBhoZSZnsYGk6N11U4Gy/view" target="_blank">
-                <img src="{{ asset('storage/p1.jpg') }}" alt="" class="rounded-t">
-            </a>
+    <div class="max-w-screen-xl mx-auto px-10 space-y-10">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-10">
+            <div class="hover:shadow-xl hover:cursor-pointer">
+                <a href="https://drive.google.com/file/d/1jug_MwnbOQYaeBhoZSZnsYGk6N11U4Gy/view" target="_blank">
+                    <img src="{{ asset('storage/p1.jpg') }}" alt="" class="rounded-t">
+                </a>
 
-            <div class="px-5 py-3 bg-stone-200 rounded-b">
-                <h4 class="uppercase font-medium text-gray-800">Học làm vợ chồng</h4>
+                <div class="px-5 py-3 bg-stone-200 rounded-b">
+                    <h4 class="uppercase font-medium text-gray-800">Học làm vợ chồng</h4>
+                </div>
+            </div>
+            <div class="hover:shadow-xl hover:cursor-pointer">
+                <a href="https://drive.google.com/file/d/1ib62jM5TdF5GusQu_ZrFulraW5c_bLQj/view" target="_blank">
+                    <img src="{{ asset('storage/p2.jpg') }}" alt="" class="rounded-t">
+                </a>
+
+                <div class="px-5 py-3 bg-stone-200 rounded-b">
+                    <h4 class="uppercase font-medium text-gray-800">Viral Tiktok Drama</h4>
+                </div>
+            </div>
+            <div class="hover:shadow-xl hover:cursor-pointer">
+                <a href="https://drive.google.com/file/d/1ppBFOJd4YHcbv1pb44wtz41PVSf7SJTZ/view" target="_blank">
+                    <img src="{{ asset('storage/p3.jpg') }}" alt="" class="rounded-t">
+                </a>
+
+                <div class="px-5 py-3 bg-stone-200 rounded-b">
+                    <h4 class="uppercase font-medium text-gray-800">Ngọc Daily</h4>
+                </div>
+            </div>
+            <div class="hover:shadow-xl hover:cursor-pointer">
+                <a href="https://drive.google.com/file/d/1V21rp_KEz0P2GlUeROGh5xUyz937QrME/view" target="_blank">
+                    <img src="{{ asset('storage/p4.jpg') }}" alt="" class="rounded-t">
+                </a>
+
+                <div class="px-5 py-3 bg-stone-200 rounded-b">
+                    <h4 class="uppercase font-medium text-gray-800">Bất động sản Review</h4>
+                </div>
             </div>
         </div>
-        <div class="hover:shadow-xl hover:cursor-pointer">
-            <a href="https://drive.google.com/file/d/1ib62jM5TdF5GusQu_ZrFulraW5c_bLQj/view" target="_blank">
-                <img src="{{ asset('storage/p2.jpg') }}" alt="" class="rounded-t">
-            </a>
 
-            <div class="px-5 py-3 bg-stone-200 rounded-b">
-                <h4 class="uppercase font-medium text-gray-800">Viral Tiktok Drama</h4>
+        <div class="flex flex-col md:flex-row gap-10 px-0 md:px-20">
+            <div class="hover:shadow-xl hover:cursor-pointer">
+                <a href="https://drive.google.com/file/d/15KdfMc5ATfe-9u3axa9GqYE1D3ypmsEQ/view" target="_blank">
+                    <img src="{{ asset('storage/p5.jpg') }}" alt="" class="rounded-t">
+                </a>
+
+                <div class="px-5 py-3 bg-stone-200 rounded-b">
+                    <h4 class="uppercase font-medium text-gray-800">QC doanh thu 1tỷ2</h4>
+                </div>
             </div>
-        </div>
-        <div class="hover:shadow-xl hover:cursor-pointer">
-            <a href="https://drive.google.com/file/d/1ppBFOJd4YHcbv1pb44wtz41PVSf7SJTZ/view" target="_blank">
-                <img src="{{ asset('storage/p3.jpg') }}" alt="" class="rounded-t">
-            </a>
+            <div class="hover:shadow-xl hover:cursor-pointer">
+                <a href="https://drive.google.com/file/d/19RdpdO2APCq70kNFR_Gtw1EMLQCr6LRn/view" target="_blank">
+                    <img src="{{ asset('storage/p6.jpg') }}" alt="" class="rounded-t">
+                </a>
 
-            <div class="px-5 py-3 bg-stone-200 rounded-b">
-                <h4 class="uppercase font-medium text-gray-800">Ngọc Daily</h4>
+                <div class="px-5 py-3 bg-stone-200 rounded-b">
+                    <h4 class="uppercase font-medium text-gray-800">TVC Thẩm mỹ viện</h4>
+                </div>
             </div>
-        </div>
-        <div class="hover:shadow-xl hover:cursor-pointer">
-            <a href="https://drive.google.com/file/d/1V21rp_KEz0P2GlUeROGh5xUyz937QrME/view" target="_blank">
-                <img src="{{ asset('storage/p4.jpg') }}" alt="" class="rounded-t">
-            </a>
+            <div class="hover:shadow-xl hover:cursor-pointer">
+                <a href="https://drive.google.com/file/d/1Hz42k1xjeyCiC9H54fulapYfTDh-aYKi/view" target="_blank">
+                    <img src="{{ asset('storage/p7.jpg') }}" alt="" class="rounded-t">
+                </a>
 
-            <div class="px-5 py-3 bg-stone-200 rounded-b">
-                <h4 class="uppercase font-medium text-gray-800">Bất động sản Review</h4>
+                <div class="px-5 py-3 bg-stone-200 rounded-b">
+                    <h4 class="uppercase font-medium text-gray-800">Nghệ sĩ review</h4>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/components/services.blade.php
+++ resources/views/components/services.blade.php
@@ -14,14 +14,16 @@
 
                 </div>
 
-                <div class="absolute w-full p-5 space-y-3 text-center">
-                    <div class="flex justify-center text-stone-300 group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
-                    </div>
+                <div class="absolute w-full">
+                    <a href="https://drive.google.com/drive/folders/1evwngg9ZZFRiMsptaegXPXoxRLVnhTEr" class="text-center p-5 space-y-3" target="_blank">
+                        <div class="flex justify-center text-stone-300 group-hover:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
+                        </div>
 
-                    <h4 class="uppercase text-2xl text-white">Sản xuất TVC quảng cáo</h4>
+                        <h4 class="uppercase text-2xl text-white">Sản xuất TVC quảng cáo</h4>
 
-                    <p class="text-gray-100 text-sm px-5 uppercase">Dịch vụ sản xuất TVC quảng cáo lợi ích của phim TVC quảng cáo</p>
+                        <p class="text-gray-100 text-sm px-5 uppercase">Dịch vụ sản xuất TVC quảng cáo lợi ích của phim TVC quảng cáo</p>
+                    </a>
                 </div>
             </div>
             <div class="rounded text-center space-y-3 relative h-56 hover:cursor-pointer hover:shadow-xl group">
@@ -29,14 +31,16 @@
 
                 </div>
 
-                <div class="absolute w-full p-5 space-y-3 text-center">
-                    <div class="flex justify-center text-stone-300 group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
-                    </div>
+                <div class="absolute w-full">
+                    <a href="https://drive.google.com/drive/folders/1eBNuN2VYSmY11z32ztFvxD3K1lhSTrgl" class="text-center p-5 space-y-3" target="_blank">
+                        <div class="flex justify-center text-stone-300 group-hover:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
+                        </div>
 
-                    <h4 class="uppercase text-2xl text-white">Tiểu phẩm quảng cáo</h4>
+                        <h4 class="uppercase text-2xl text-white">Tiểu phẩm quảng cáo</h4>
 
-                    <p class="text-gray-100 text-sm px-5 uppercase">Dịch vụ sản xuất tiểu phẩm quảng cáo</p>
+                        <p class="text-gray-100 text-sm px-5 uppercase">Dịch vụ sản xuất tiểu phẩm quảng cáo</p>
+                    </a>
                 </div>
             </div>
             <div class="rounded text-center space-y-3 relative h-56 hover:cursor-pointer hover:shadow-xl group">
@@ -44,14 +48,16 @@
 
                 </div>
 
-                <div class="absolute w-full p-5 space-y-3 text-center">
-                    <div class="flex justify-center text-stone-300 group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
-                    </div>
+                <div class="absolute w-full">
+                    <a href="https://drive.google.com/drive/folders/1MTwcwIbSOXs2KMpLebnKKZ_FjnlTN-0M" class="text-center p-5 space-y-3" target="_blank">
+                        <div class="flex justify-center text-stone-300 group-hover:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
+                        </div>
 
-                    <h4 class="uppercase text-2xl text-white">Xây kênh tiktok</h4>
+                        <h4 class="uppercase text-2xl text-white">Xây kênh tiktok</h4>
 
-                    <p class="text-gray-100 text-sm px-5 uppercase">Dịch vụ xây kênh, video tiktok</p>
+                        <p class="text-gray-100 text-sm px-5 uppercase">Dịch vụ xây kênh, video tiktok</p>
+                    </a>
                 </div>
             </div>
             <div class="rounded text-center space-y-3 relative h-56 hover:cursor-pointer hover:shadow-xl group">
@@ -59,14 +65,16 @@
 
                 </div>
 
-                <div class="absolute w-full p-5 space-y-3 text-center">
-                    <div class="flex justify-center text-stone-300 group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
-                    </div>
+                <div class="absolute w-full">
+                    <a href="https://drive.google.com/drive/folders/1CZNr7h7b6Q2YfOcBiRuuKFy3WxUaLP-r" class="text-center p-5 space-y-3" target="_blank">
+                        <div class="flex justify-center text-stone-300 group-hover:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
+                        </div>
 
-                    <h4 class="uppercase text-2xl text-white">KOC/KOLS review sản phẩm</h4>
+                        <h4 class="uppercase text-2xl text-white">KOC/KOLS review sản phẩm</h4>
 
-                    <p class="text-gray-100 text-sm px-5 uppercase">Cung cấp dịch vụ review sản phẩm</p>
+                        <p class="text-gray-100 text-sm px-5 uppercase">Cung cấp dịch vụ review sản phẩm</p>
+                    </a>
                 </div>
             </div>
             <div class="rounded text-center space-y-3 relative h-56 hover:cursor-pointer hover:shadow-xl group">
@@ -74,14 +82,16 @@
 
                 </div>
 
-                <div class="absolute w-full p-5 space-y-3 text-center">
-                    <div class="flex justify-center text-stone-300 group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
-                    </div>
+                <div class="absolute w-full">
+                    <a href="https://drive.google.com/drive/folders/1cVZSax5Jpr6hXQsNrNUifG3eslUiRpBv" class="text-center p-5 space-y-3" target="_blank">
+                        <div class="flex justify-center text-stone-300 group-hover:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
+                        </div>
 
-                    <h4 class="uppercase text-2xl text-white">Clip Viral</h4>
+                        <h4 class="uppercase text-2xl text-white">Clip Viral</h4>
 
-                    <p class="text-gray-100 text-sm px-5 uppercase">Cung cấp dịch vụ sản xuất phim viral video</p>
+                        <p class="text-gray-100 text-sm px-5 uppercase">Cung cấp dịch vụ sản xuất phim viral video</p>
+                    </a>
                 </div>
             </div>
             <div class="rounded text-center space-y-3 relative h-56 hover:cursor-pointer hover:shadow-xl group">
@@ -89,14 +99,16 @@
 
                 </div>
 
-                <div class="absolute w-full p-5 space-y-3 text-center">
-                    <div class="flex justify-center text-stone-300 group-hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
-                    </div>
+                <div class="absolute w-full">
+                    <a href="https://drive.google.com/drive/folders/1fsGHe6Tqtnk5lfTcz5B-npR29K0938om" class="text-center p-5 space-y-3" target="_blank">
+                        <div class="flex justify-center text-stone-300 group-hover:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-movie" width="50" height="50" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" /></svg>
+                        </div>
 
-                    <h4 class="uppercase text-2xl text-white">Dịch vụ hậu kỳ video</h4>
+                        <h4 class="uppercase text-2xl text-white">Dịch vụ hậu kỳ video</h4>
 
-                    <p class="text-gray-100 text-sm px-5 uppercase">Cung cấp dịch vụ biên tập hậu kỳ video</p>
+                        <p class="text-gray-100 text-sm px-5 uppercase">Cung cấp dịch vụ biên tập hậu kỳ video</p>
+                    </a>
                 </div>
             </div>
         </div>
