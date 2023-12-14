9e8606b4bc845d004c501e9e9429991bdae8f14d
dongthanhlx <dongthanhlx@gmail.com>
2023-12-14 15:09:40 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="icon" href="{{ asset('storage/g84-logo.png') }}"
+          type="image/x-icon">
 </head>
 <body class="relative">
 @include('components.header')
